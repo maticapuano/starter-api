2dade6bfc1901f19969cc2e64a3b40e8c729b31c
Matías Capuano <maticapuano97@gmail.com>
2020-11-26 15:54:57 -0600

    [REFACTOR] remove env not nesesary.

--- src/config/secrets.env.ts
+++ src/config/secrets.env.ts
@@ -1,20 +1,12 @@
 export const NODE_ENV: string = process.env.NODE_ENV || 'development';
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 3000;
 export const CORS_DOMAIN = process.env.CORS_DOMAIN;
-export const DB_HOST = process.env.DB_HOST;
-export const DB_USER = process.env.DB_USER;
-export const DB_PORT = process.env.DB_PORT;
+export const DB_URI = process.env.DB_URI;
 export const ACCESS_TOKEN_EXP_DAYS = process.env.ACCESS_TOKEN_EXP_DAYS;
 export const REFRESH_TOKEN_EXP_DAYS =
   process.env.REFRESH_TOKEN_EXP_DAYS;
 export const TOKEN_ISSUER = process.env.TOKEN_ISSUER;
 export const TOKEN_AUDIENCE = process.env.TOKEN_AUDIENCE;
-
-if (!PORT) {
-  console.log('[PORT] not has defined.');
-
-  process.exit(1);
-}
 
 if (!CORS_DOMAIN) {
   console.log('[CORS_DOMAIN] not has defined');
@@ -22,14 +14,8 @@ if (!CORS_DOMAIN) {
   process.exit(1);
 }
 
-if (!DB_HOST) {
-  console.log('[DB_HOST] not has defined');
-
-  process.exit(1);
-}
-
-if (!DB_USER) {
-  console.log('[DB_USER] not has defined');
+if (!DB_URI) {
+  console.log('[DB_URI] not has defined');
 
   process.exit(1);
 }
