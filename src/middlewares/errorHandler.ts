33e8fab6b48cf66e027bef476e8e93caae3d7a41
Matías Capuano <maticapuano97@gmail.com>
2020-11-26 17:48:38 -0600

    [ADD] error handler.

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,29 @@
+import { logger } from '@config/logger';
+import { NODE_ENV } from '@config/secrets.env';
+import { ApiError } from '@errors/ApiError';
+import { Request, Response, NextFunction } from 'express';
+import httpStatus from 'http-status-codes';
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (err instanceof ApiError) {
+    return ApiError.handler(err, res);
+  }
+
+  if (NODE_ENV === 'development') {
+    logger.error(err);
+
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: err.message,
+      error: err.name,
+      stack: err.stack,
+    });
+
+    // Todo: internal error
+  }
+};
