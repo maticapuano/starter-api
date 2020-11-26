bc3be1e7a1af24fcc022c53ea1d716b72b295734
Matías Capuano <maticapuano97@gmail.com>
2020-11-26 17:11:07 -0600

    [ADD] Added encoded request in json & enabled cors.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,30 @@
 import '@config/dotenv';
+import { CORS_DOMAIN, NODE_ENV } from '@config/secrets.env';
 import express, { Application } from 'express';
+import morgan from 'morgan';
+import compression from 'compression';
+import cors from 'cors';
+import mongoSanitize from 'express-mongo-sanitize';
+import helmet from 'helmet';
 
 const app: Application = express();
 
-app.get('/', (req, res) => res.json({ work: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Compress request
+app.use(compression());
+
+// Set cors
+app.options('*', cors({ origin: CORS_DOMAIN }));
+
+// Prevent injection NoSql
+app.use(mongoSanitize());
+
+app.use(helmet());
 
 export { app };
