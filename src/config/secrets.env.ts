79d99fecdac3de50f6d30f2581687d974f2827cb
Matías Capuano <maticapuano97@gmail.com>
2020-11-26 15:27:07 -0600

    [ADD] force quit application if env not defined.

--- src/config/secrets.env.ts
+++ src/config/secrets.env.ts
@@ -18,25 +18,39 @@ if (!PORT) {
 
 if (!CORS_DOMAIN) {
   console.log('[CORS_DOMAIN] not has defined');
+
+  process.exit(1);
 }
 
 if (!DB_HOST) {
   console.log('[DB_HOST] not has defined');
+
+  process.exit(1);
 }
 
 if (!DB_USER) {
   console.log('[DB_USER] not has defined');
+
+  process.exit(1);
 }
 
 if (!ACCESS_TOKEN_EXP_DAYS) {
   console.log('[ACCESS_TOKEN_EXP_DAYS] not has defined');
+
+  process.exit(1);
 }
 if (!REFRESH_TOKEN_EXP_DAYS) {
   console.log('[REFRESH_TOKEN_EXP_DAYS] not has defined');
+
+  process.exit(1);
 }
 if (!TOKEN_ISSUER) {
   console.log('[TOKEN_ISSUER] not has defined');
+
+  process.exit(1);
 }
 if (!TOKEN_AUDIENCE) {
   console.log('[TOKEN_AUDIENCE] not has defined');
+
+  process.exit(1);
 }
