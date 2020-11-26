8f39d03f4565240f6d44d8a4b40b55103040ba99
Matías Capuano <maticapuano97@gmail.com>
2020-11-26 17:12:42 -0600

    [ADD] implemented mongodb in server express.

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,21 @@ import 'tsconfig-paths/register';
 import { app } from '@app';
 import { logger } from '@config/logger';
 import { PORT } from '@config/secrets.env';
+import { initDatabase } from '@config/database';
 
-app.listen(PORT, () => logger.info(`Server ready on port ${PORT}`));
+const start = async () => {
+  try {
+    const database = await initDatabase();
+
+    if (database) {
+      app.listen(PORT, () =>
+        logger.info(`Server ready on port ${PORT}`),
+      );
+    }
+  } catch (error) {
+    logger.fatal(`An exception ocurred ${error}`);
+    process.exit(1);
+  }
+};
+
+start();
