1e05e9584293a50f83bcc39f41993b60beb21ff6
Matías Capuano <maticapuano97@gmail.com>
2020-11-27 12:01:17 -0600

    [ADD] Hashed & comprare passowrd encryptation.

---
+++ src/utils/Encryption.ts
@@ -0,0 +1,27 @@
+import { randomBytes, scrypt } from 'crypto';
+import { promisify } from 'util';
+
+const scryptAsync = promisify(scrypt);
+
+export class Encryption {
+  static async toHash(password: string): Promise<string> {
+    const salt = randomBytes(14).toString('hex');
+    const buffer = (await scryptAsync(password, salt, 64)) as Buffer;
+
+    return `${buffer.toString('hex')}.${salt}`;
+  }
+
+  static async compare(
+    storedPassword: string,
+    suppliedPassword: string,
+  ): Promise<boolean> {
+    const [hashedPassword, salt] = storedPassword.split('.');
+    const buffer = (await scryptAsync(
+      suppliedPassword,
+      salt,
+      64,
+    )) as Buffer;
+
+    return buffer.toString('hex') === hashedPassword;
+  }
+}
