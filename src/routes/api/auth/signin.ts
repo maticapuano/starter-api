0f996ac9bb0d999d6c18f5c268316917bdad0c85
Matías Capuano <maticapuano97@gmail.com>
2020-11-27 08:30:03 -0600

    [ADD] implemented schema validation signIn

--- src/routes/api/auth/signin.ts
+++ src/routes/api/auth/signin.ts
@@ -1,10 +1,28 @@
+import { validateRequest } from '@middlewares/validateRequest';
 import { ApiResponse } from '@utils/ApiResponse';
 import { Request, Response, Router } from 'express';
+import { body } from 'express-validator';
 
 const router = Router();
 
-router.post('/auth/signin', (req: Request, res: Response) => {
-  return ApiResponse.success(res, { route: 'Signin user' });
-});
+const signInSchema = [
+  body('email')
+    .isEmail()
+    .normalizeEmail({ all_lowercase: true })
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .isLength({ min: 4, max: 50 })
+    .withMessage('Password must be between 4 and 50 characters.'),
+];
+
+router.post(
+  '/auth/signin',
+  signInSchema,
+  validateRequest,
+  (req: Request, res: Response) => {
+    return ApiResponse.success(res, { route: 'Signin user' });
+  },
+);
 
 export { router as signInRoute };
