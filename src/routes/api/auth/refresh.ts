cdde9e6bee1aefb151547754ce873e87c256630a
Matías Capuano <maticapuano97@gmail.com>
2020-11-27 11:19:02 -0600

    [ADD] refresh token schema validation

--- src/routes/api/auth/refresh.ts
+++ src/routes/api/auth/refresh.ts
@@ -1,10 +1,24 @@
+import { validateRequest } from '@middlewares/validateRequest';
 import { ApiResponse } from '@utils/ApiResponse';
 import { Request, Response, Router } from 'express';
+import { header } from 'express-validator';
 
 const router = Router();
 
-router.post('/auth/refresh', (req: Request, res: Response) => {
-  return ApiResponse.success(res, { route: 'Refresh token' });
-});
+const refreshTokenSchema = [
+  header('refreshToken')
+    .isJWT()
+    .notEmpty()
+    .withMessage('Refresh token is not valid.'),
+];
+
+router.post(
+  '/auth/refresh',
+  refreshTokenSchema,
+  validateRequest,
+  (req: Request, res: Response) => {
+    return ApiResponse.success(res, { route: 'Refresh token' });
+  },
+);
 
 export { router as refreshTokenRoute };
