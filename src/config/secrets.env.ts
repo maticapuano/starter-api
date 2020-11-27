5af7e39a8f57b060a43c8c6eb8ba1456e8026014
Matías Capuano <maticapuano97@gmail.com>
2020-11-27 14:15:19 -0600

    [ADD] access token env secret.

--- src/config/secrets.env.ts
+++ src/config/secrets.env.ts
@@ -8,6 +8,7 @@ export const REFRESH_TOKEN_EXP_DAYS =
 export const TOKEN_ISSUER = process.env.TOKEN_ISSUER;
 export const TOKEN_AUDIENCE = process.env.TOKEN_AUDIENCE;
 export const ROUTE_PREFIX = process.env.ROUTE_PREFIX || 'v1';
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
 if (!CORS_DOMAIN) {
   console.log('[CORS_DOMAIN] not has defined');
@@ -38,6 +39,12 @@ if (!TOKEN_ISSUER) {
 }
 if (!TOKEN_AUDIENCE) {
   console.log('[TOKEN_AUDIENCE] not has defined');
+
+  process.exit(1);
+}
+
+if (!ACCESS_TOKEN_SECRET) {
+  console.log('[ACCESS_TOKEN_SECRET] not has defined');
 
   process.exit(1);
 }
