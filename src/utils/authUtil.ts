bacf24cda481802fc9de2a2d39827e0b41a5e37c
Matías Capuano <maticapuano97@gmail.com>
2020-11-27 14:18:17 -0600

    [ADD] generate tokens before signIn.

---
+++ src/utils/authUtil.ts
@@ -0,0 +1,31 @@
+import {
+  ACCESS_TOKEN_EXP_DAYS,
+  REFRESH_TOKEN_EXP_DAYS,
+  TOKEN_AUDIENCE,
+  TOKEN_ISSUER,
+} from '@config/secrets.env';
+import { IUserDto } from '@models/User.model';
+import { JWT } from './JWT';
+
+interface Tokens {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export const createTokens = async (user: IUserDto): Promise<Tokens> => {
+  const accessToken = await JWT.encode(user, {
+    issuer: TOKEN_ISSUER,
+    audience: TOKEN_AUDIENCE,
+    expiresIn: ACCESS_TOKEN_EXP_DAYS,
+  });
+  const refreshToken = await JWT.encode(user, {
+    issuer: TOKEN_ISSUER,
+    audience: TOKEN_AUDIENCE,
+    expiresIn: REFRESH_TOKEN_EXP_DAYS,
+  });
+
+  return {
+    accessToken: accessToken,
+    refreshToken: refreshToken,
+  } as Tokens;
+};
