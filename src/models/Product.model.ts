25bd73892998bf96eec565d2b2b38569453242d7
Matías Capuano <maticapuano97@gmail.com>
2020-11-28 17:20:56 -0600

    [ADD] schema product.

---
+++ src/models/Product.model.ts
@@ -0,0 +1,61 @@
+import mongoose from 'mongoose';
+
+export interface IProductDto {
+  id?: string;
+  name: string;
+  description: string;
+  stock?: number;
+  userId: string;
+  isActive?: boolean;
+}
+
+export interface ProductDoc extends mongoose.Document {
+  name: string;
+  description: string;
+  stock: number;
+  userId: string;
+  isActive?: boolean;
+}
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      lowercase: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      lowercase: true,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  },
+);
+
+export const Product = mongoose.model<ProductDoc>(
+  'Products',
+  productSchema,
+);
