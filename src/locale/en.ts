32e88a101bbd13308ca9bcaa6a7d5502d7f3160c
Matías Capuano <maticapuano97@gmail.com>
2020-11-28 13:35:00 -0600

    [ADD] locale english for responses.

---
+++ src/locale/en.ts
@@ -0,0 +1,9 @@
+export default {
+  EMAIL_ALREADY_EXISTS:
+    'Email already exists. Please login to continue',
+  ACCOUNT_NOT_ACTIVATED: 'Account not activated',
+  CREDENTIALS_INVALID: 'Email or Password is invalid.',
+  ACCOUNT_CREATED_SUCCESS: 'Account created successfully',
+  LOGIN_SUCCESS: 'Login successfully',
+  SHOWING_CURRENT_USER: 'Showing user details.',
+};
